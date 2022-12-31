b1cf1ad38f4d35f5a00b84867b3ae864c8e0ecef
Mário Alves <mario.alves@superopa.com>
2022-12-31 01:20:38 -0300

    Up

--- protocol and delegate/main.swift
+++ protocol and delegate/main.swift
@@ -1,11 +1,48 @@
-//
-//  main.swift
-//  protocol and delegate
-//
-//  Created by Eydde on 31/12/22.
-//
 
-import Foundation
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
 
-print("Hello, World!")
 
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The paramedic does the chest compressions, 30 per second.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The Doctor does the chest compressions, 30 per second.")
+        
+    }
+    
+}
+
+
+let emilio = EmergencyCallHandler()
+let mario = Doctor(handler: emilio)
+//let cah = Paramedic(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
